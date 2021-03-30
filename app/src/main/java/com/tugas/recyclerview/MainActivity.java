c2354f745f00027acd43cbb548bc5f5b86c89c7f
martadana <komangmarta25@gmail.com>
2021-03-30 22:36:49 +0800

    pesan pertama

---
+++ app/src/main/java/com/tugas/recyclerview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.tugas.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images [] = {R.drawable.c_plus,R.drawable.java,R.drawable.javascripts,R.drawable.kotlin,R.drawable.python};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+
+        s1 = getResources().getStringArray(R.array.programing_laguages);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
